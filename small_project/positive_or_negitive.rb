b8ea99aade04dd57933256af73888d093d8b8d35
mulbah11k <mulbahtkolleh@gmail.com>
2025-04-23 12:47:47 +0000

    A function that check if a number is positive or negitive

---
+++ small_project/positive_or_negitive.rb
@@ -0,0 +1,13 @@
+def positive_or_negitiv(n)
+  if n < 0
+    puts "#{n} is Negitive."
+  
+  elsif n > 0
+    puts "#{n} is positive."
+  
+  else
+    puts "Is zero."
+  end
+end
+
+positive_or_negitiv 0
