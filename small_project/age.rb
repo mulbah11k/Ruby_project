8d51752409f022dce1cff968cf672ae122b0cfc0
mulbah11k <mulbahtkolleh@gmail.com>
2025-04-23 12:16:53 +0000

    Work by adding 10 to user age

---
+++ small_project/age.rb
@@ -0,0 +1,10 @@
+
+puts "How old are you?"
+real_age = gets.chomp.to_i
+
+puts "In 10 years you will be: "
+puts real_age + 10
+puts "In 20 years you will be: "
+puts real_age + 20
+puts "In 30 years you will be: "
+puts real_age + 30
